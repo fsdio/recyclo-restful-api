fb12654703272b2dab89174f1630f3907d4ccfb8
Syahda Romansyah <syahdarom@gmail.com>
2023-05-22 21:20:00 +0700

    refactor: change waste handlers filename initialization

--- src/handlers/recyclo-handlers.ts
+++ src/handlers/recyclo-handlers.ts
@@ -1,6 +1,6 @@
-import addWaste from './recyclo-handlers/waste-handlers/addWaste.js';
-import deleteWaste from './recyclo-handlers/waste-handlers/deleteWaste.js';
-import getWastes from './recyclo-handlers/waste-handlers/getWastes.js';
-import putWaste from './recyclo-handlers/waste-handlers/putWaste.js';
+import addWaste from './recyclo-handlers/waste-handlers/add-waste.js';
+import deleteWaste from './recyclo-handlers/waste-handlers/delete-waste.js';
+import getWastes from './recyclo-handlers/waste-handlers/get-wastes.js';
+import putWaste from './recyclo-handlers/waste-handlers/put-waste.js';
 
 export { getWastes, addWaste, putWaste, deleteWaste };
